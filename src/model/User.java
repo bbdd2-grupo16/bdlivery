9b3a8cf37666283d183b2b92cb471ec819ef43b7
Marianela <marianelgau@gmail.com>
2020-03-29 16:17:12 -0300

    Model user

--- src/model/User.java
+++ src/model/User.java
@@ -1,9 +1,81 @@
+@Entity(name="User")
 public class User{
 
+     @Id
+     private String id;
+     private String username; /*nombre de usuario del usuario*/
      private String email; /*email del usuario*/
      private String password; /*password clave del usuario*/
-     private String username; /*nombre de usuario del usuario*/
      private String name; /*nombre y apellido del usuario*/
      private Date dateOfBirth; /*fecha de nacimiento del usuario*/
+     @OneToMany(cascade=CascadeType.ALL, orphanRemoval=true)
+     @JoinColumn(name="user_id")
+     private Order orders = new ArrayList<>();
 
+     public String getId() {
+          return id;
+     }
+
+     public void setId(String id) {
+          this.id = id;
+     }
+
+     public String getEmail() {
+          return email;
+     }
+
+     public void setEmail(String email) {
+          this.email = email;
+     }
+
+     public String getPassword() {
+          return password;
+     }
+
+     public void setPassword(String password) {
+          this.password = password;
+     }
+
+     public String getUsername() {
+          return username;
+     }
+
+     public void setUsername(String username) {
+          this.username = username;
+     }
+
+     public String getName() {
+          return name;
+     }
+
+     public void setName(String name) {
+          this.name = name;
+     }
+
+     public Date getDateOfBirth() {
+          return dateOfBirth;
+     }
+
+     public void setDateOfBirth(Date dateOfBirth) {
+          this.dateOfBirth = dateOfBirth;
+     }
+
+     public Order getOrders() {
+          return orders;
+     }
+
+     public void setOrders(Order orders) {
+          this.orders = orders;
+     }
+
+     public User() {
+     }
+
+     public User(String email, String password, String username, String name, Date dateOfBirth) {
+          this.email = email;
+          this.password = password;
+          this.username = username;
+          this.name = name;
+          this.dateOfBirth = dateOfBirth;
+     }
 }
