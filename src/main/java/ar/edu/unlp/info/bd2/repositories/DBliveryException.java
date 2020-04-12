699ac6383d69168afa272ebedd6a04b683fe3479
Marianela <marianelgau@gmail.com>
2020-04-12 16:11:01 -0300

    Agregados nuevos archivos para el trabajo nro 2

---
+++ src/main/java/ar/edu/unlp/info/bd2/repositories/DBliveryException.java
@@ -0,0 +1,9 @@
+
+package ar.edu.unlp.info.bd2.repositories;
+
+public class DBliveryException extends Exception {
+
+    public DBliveryException(String message) {
+        super(message);
+    }
+}
