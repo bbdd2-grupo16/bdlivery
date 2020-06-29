14c6b7839a7a1fad129b0aca6d3a71cce526900b
echeva <yaniiecheva@gmail.com>
2020-06-28 22:28:02 -0300

    Pasan todos los de product

--- src/main/java/ar/edu/unlp/info/bd2/repositories/ProductOrderRepository.java
+++ src/main/java/ar/edu/unlp/info/bd2/repositories/ProductOrderRepository.java
@@ -1,8 +1,19 @@
 package ar.edu.unlp.info.bd2.repositories;
+import ar.edu.unlp.info.bd2.model.Product;
 import ar.edu.unlp.info.bd2.model.ProductOrder;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Repository;
+
+import java.util.Date;
+import java.util.List;
 
 @Repository
 public interface ProductOrderRepository extends CrudRepository<ProductOrder, Long> {
+
+    @Nullable
+    @Query("SELECT DISTINCT po.product FROM ProductOrder AS po WHERE po.order.dateOfOrder = :day")
+    List<Product> getSoldProductsOn(@Param("day") Date day);
 }
